8af73f23260ee743c7f7daf524f6b3323f9e3421
Andrey Aratov <beg431@gmail.com>
2022-07-28 22:48:32 +0300

    added db connection

--- server/index.js
+++ server/index.js
@@ -1,7 +1,21 @@
 const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const PORT = process.env.PORT || 3000;
-
+const sequelize = require('./db');
 const app = express();
 
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+const startServer = async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+
+    app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+startServer();

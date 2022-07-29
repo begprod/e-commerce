5c731e5c0fd967ee8e6ed09933018e01abcf9d50
Andrey Aratov <beg431@gmail.com>
2022-07-29 23:14:19 +0300

    added models

--- server/index.js
+++ server/index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 const sequelize = require('./db');
+const models = require('./models/models');
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Server is running' });
+});
 
 const startServer = async () => {
   try {

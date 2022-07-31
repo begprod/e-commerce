3825c1f1a77996ef22040da90cf4d483ba4442f5
Andrey Aratov <beg431@gmail.com>
2022-07-31 22:48:15 +0300

    added create, getall funcs for type, brand

--- server/routes/index.js
+++ server/routes/index.js
@@ -8,6 +8,6 @@ const brandRouter = require('./brandRouter');
 router.use('/user', userRouter);
 router.use('/device', deviceRouter);
 router.use('/type', typeRouter);
-router.use('/brand ', brandRouter);
+router.use('/brand', brandRouter);
 
 module.exports = router;

66f4287f672ede7e7477503b40d53b4c9ca85c66
Andrey Aratov <beg431@gmail.com>
2022-08-02 21:41:08 +0300

    added package for fileupload

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const fileUpload = require('express-fileupload');
 
 dotenv.config();
 
@@ -13,6 +14,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(fileUpload({}));
 app.use('/api', router);
 app.use(errorHandler);
 

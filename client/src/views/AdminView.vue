cfa9d471bbba362e5deba0735b7bf899c3c34090
Andrey Aratov <beg431@gmail.com>
2022-09-17 21:37:18 +0300

    added types, brands state to admin form

--- client/src/views/AdminView.vue
+++ client/src/views/AdminView.vue
@@ -81,9 +81,13 @@
             required
           >
             <option value="">Select type</option>
-            <option value="1" label="Phone">Phone</option>
-            <option value="2" label="Laptop">Laptop</option>
-            <option value="3" label="Tablet">Tablet</option>
+            <option
+              v-for="type in types"
+              :key="type.id"
+              :value="type.id"
+            >
+              {{ type.name }}
+            </option>
           </BaseSelect>
         </fieldset>
         <fieldset>
@@ -94,9 +98,13 @@
             required
           >
             <option value="">Select brand</option>
-            <option value="1" label="Apple">Apple</option>
-            <option value="2" label="Samsung">Samsung</option>
-            <option value="3" label="Xiaomi">Xiaomi</option>
+            <option
+              v-for="brand in brands"
+              :key="brand.id"
+              :value="brand.id"
+            >
+              {{ brand.name }}
+            </option>
           </BaseSelect>
         </fieldset>
         <fieldset>
@@ -121,7 +129,9 @@
 </template>
 
 <script setup>
+import { computed } from 'vue';
 import { useMeta } from 'vue-meta';
+import useProductStore from '@/stores/products';
 import BaseFormWrapper from '@/components/ui/forms/BaseFormWrapper.vue';
 import BaseInput from '@/components/ui/forms/BaseInput.vue';
 import BaseSelect from '@/components/ui/forms/BaseSelect.vue';
@@ -130,6 +140,9 @@ import BaseButton from '@/components/ui/forms/BaseButton.vue';
 useMeta({
   title: 'Admin panel',
 });
+
+const types = computed(() => useProductStore().getTypes);
+const brands = computed(() => useProductStore().getBrands);
 </script>
 
 <style lang="postcss" scoped>
